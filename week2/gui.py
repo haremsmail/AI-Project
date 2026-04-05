e0b61adac0cbc643accff634e7ace083e34c3ef5
srwshhoger <srwsh.200508473111@gmail.com>
2026-04-05 20:53:52 +0300

    finsh my week2 task

--- week2/gui.py
+++ week2/gui.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox, font
 import threading
-""" runing task in backgroun"""
 from board import Board
 from vacuum_dfs import VacuumDFS
 """" wata import dipth first serach alogoithm"""
